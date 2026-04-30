c081b8cf1fede802c9f6fee6241f968f67808642
ahmed <ahmed@gmail.com>
2026-04-30 10:39:00 +0300

    scroll problem solving

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ const App = () => {
   document.body.className = themes;
   return (
     <>
-      <header className="bg-white/50 dark:bg-gray-900/75 z-20 transition-all sticky top-0 backdrop-blur-xl">
+      <header className="bg-white/50 dark:bg-gray-900/75 z-20 transition-all fixed w-full top-0 backdrop-blur-xl">
         <NavBar themes={themes} setThemes={() => setThemes(prev => prev === 'light'? 'dark':'light')}/>
       </header>
-      <main>
+      <main className="pt-17">
         <section className="dark:bg-black relative pt-10 transition-all">
           <Hero />
         </section>
