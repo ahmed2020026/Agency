e72b99ae7bf51576212d31bf04e1e16160e2e8ac
ahmed <ahmed@gmail.com>
2026-05-02 15:32:36 +0300

    OPT

--- src/componants/Hero.tsx
+++ src/componants/Hero.tsx
@@ -10,7 +10,8 @@ const Hero = () => {
             <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-[84px] max-w-5xl">Turning imagination into <span className="bg-gradient-to-r from-[#5044E5] to-[#4d8cea] bg-clip-text text-transparent">digital</span> impact.</h1>
             <p className="text-sm sm:text-lg font-medium text-gray-700 dark:text-gray-500 max-w-4/5 sm:max-w-lg pb-3">Creating meaningful connections and turning big ideas into interactive digital experiences.</p>
             <div className="relative">
-                <img src={assets.hero_img} width={1200} height={600} fetchPriority="high" className="w-full max-w-6xl" alt="Hero_image" />
+                <img src={assets.hero_img}
+                    sizes="(max-width: 600px) 400px, 800px" fetchPriority="high" className="w-full max-w-6xl" alt="Hero_image" />
             </div>
         </div>
     )
